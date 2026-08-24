b0427d47aceeb4ad77a6afc66c95c82f4944ed2f
Napodji <sasha.k7@mail.ru>
2026-08-24 15:09:23 +0300

    HW08: review fixes (move meals view rendering back to RootController)

--- src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
@@ -2,8 +2,6 @@ package ru.javawebinar.topjava.web.meal;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.Nullable;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
@@ -13,18 +11,11 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/meals")
 public class MealUIController extends AbstractMealController {
 
-    @GetMapping
-    public String getAll(Model model) {
-        model.addAttribute("meals", super.getAll());
-        return "meals";
-    }
-
     @PostMapping
-    @ResponseBody
     public Meal create(@RequestParam String dateTime,
                        @RequestParam String description,
                        @RequestParam int calories) {
@@ -32,14 +23,12 @@ public class MealUIController extends AbstractMealController {
     }
 
     @DeleteMapping("/{id}")
-    @ResponseBody
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable int id) {
         super.delete(id);
     }
 
     @GetMapping("/filter")
-    @ResponseBody
     public List<MealTo> getBetween(
             @RequestParam @Nullable LocalDate startDate,
             @RequestParam @Nullable LocalTime startTime,
